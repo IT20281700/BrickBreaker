05d3af57b76ae93beac691073f94fc6b68b545fc
IT20281700 <it20281700@my.sliit.lk>
2022-06-04 12:56:24 +0530

    generate bricks and hit using ball to it

--- src/brickBreaker/Gameplay.java
+++ src/brickBreaker/Gameplay.java
@@ -2,6 +2,7 @@ package brickBreaker;
 
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.Rectangle;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -27,7 +28,11 @@ public class Gameplay extends JPanel implements KeyListener, ActionListener {
 	private int ballXdir = -2;
 	private int ballYdir = -4;
 
+	private MapGenertor map;
+
 	public Gameplay() {
+
+		map = new MapGenertor(3, 7);
 
 		addKeyListener(this);
 		setFocusable(true);
@@ -42,6 +47,9 @@ public class Gameplay extends JPanel implements KeyListener, ActionListener {
 		// background
 		g.setColor(Color.black);
 		g.fillRect(1, 1, 692, 592);
+
+		// drawing map
+		map.draw((Graphics2D) g);
 
 		// borders
 		g.setColor(Color.yellow);
@@ -62,25 +70,54 @@ public class Gameplay extends JPanel implements KeyListener, ActionListener {
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		timer.start();
-		
-		if(play) {
-			if(new Rectangle(ballposX, ballposY, 20, 20).intersects(new Rectangle(playerX, 550, 100, 8))) {
+
+		if (play) {
+			if (new Rectangle(ballposX, ballposY, 20, 20).intersects(new Rectangle(playerX, 550, 100, 8))) {
 				ballYdir = -ballYdir;
 			}
-			
+
+			A:for (int i = 0; i < map.map.length; i++) {
+				for (int j = 0; j < map.map[0].length; j++) {
+					if(map.map[i][j] > 0) {
+						int brickX = j * map.brickWidth + 80;
+						int brickY = i * map.brickHeight + 50;
+						int brickWidth = map.brickWidth;
+						int brickHeight = map.brickHeight;
+						
+						Rectangle rect = new Rectangle(brickX, brickY, brickWidth, brickHeight);
+						Rectangle ballRect = new Rectangle(ballposX, ballposY, 20, 20);
+						Rectangle brickRect = rect;
+						
+						if(ballRect.intersects(brickRect)) {
+							map.setBrickValue(0, i, j);
+							totalBricks--;
+							score += 5;
+							
+							if(ballposX + 19 <= brickRect.x || ballposX + 1 >= brickRect.x + brickRect.width) {
+								ballXdir = -ballXdir;
+							} else {
+								ballYdir = - ballYdir;
+							}
+							
+							break A;
+						}
+					}
+				}
+			}
+
 			ballposX += ballXdir;
 			ballposY += ballYdir;
-			if(ballposX < 0) {
+			if (ballposX < 0) {
 				ballXdir = -ballXdir;
 			}
-			if(ballposY < 0) {
+			if (ballposY < 0) {
 				ballYdir = -ballYdir;
 			}
-			if(ballposX > 670) {
+			if (ballposX > 670) {
 				ballXdir = -ballXdir;
 			}
 		}
-		
+
 		repaint();
 
 	}
@@ -118,15 +155,15 @@ public class Gameplay extends JPanel implements KeyListener, ActionListener {
 		}
 
 	}
-	
+
 	public void moveRight() {
 		play = true;
-		playerX+=20;
+		playerX += 20;
 	}
-	
+
 	public void moveLeft() {
 		play = true;
-		playerX-=20;
+		playerX -= 20;
 	}
 
 }
